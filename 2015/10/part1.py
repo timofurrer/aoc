3e7ace05bbe047f96584fb2180a248a81193db53
Timo Furrer <tuxtimo@gmail.com>
2021-01-03 13:17:12 +0000

    Solve 2015 Day 10

---
+++ 2015/10/part1.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+with (Path(__file__).parent / "input.txt").open() as puzzle_input_file:
+    puzzle_input_raw = puzzle_input_file.read()
+
+import itertools
+
+number = puzzle_input_raw
+
+def look_and_say(look):
+    return ''.join(str(len([1 for _ in v])) + k for k, v in itertools.groupby(look))
+
+for _ in range(40):
+    number = look_and_say(number)
+
+print(len(number))
