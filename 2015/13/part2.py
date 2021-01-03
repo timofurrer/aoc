58a14a058325f7ce070e6ae8f3b22b56bff4ce7b
Timo Furrer <tuxtimo@gmail.com>
2021-01-03 15:35:39 +0000

    Solve 2015 Day 13

---
+++ 2015/13/part2.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+with (Path(__file__).parent / "input.txt").open() as puzzle_input_file:
+    puzzle_input_raw = puzzle_input_file.read()
+
+import itertools
+import functools
+
+ratings = {(x[0], x[-1][:-1]): int(x[3]) if x[2] == "gain" else int(x[3]) * -1 for x in (x.split() for x in puzzle_input_raw.splitlines())}
+persons = set(itertools.chain(*(x for x in ratings)))
+
+# add myself
+ratings.update((x, 0) for x in itertools.product(("myself",), persons))
+ratings.update((x, 0) for x in itertools.product(persons, ("myself",)))
+persons.add("myself")
+
+overall_happiness = {}
+for seating in itertools.permutations(persons, len(persons)):
+    table_seating = seating + (seating[0],) + seating[::-1]
+    happiness = functools.reduce(lambda h, s: h + ratings[s], zip(table_seating, table_seating[1:]), 0)
+    overall_happiness[seating] = happiness
+
+maximal_happiness = max(overall_happiness.items(), key=lambda x: x[1])
+print(maximal_happiness)
