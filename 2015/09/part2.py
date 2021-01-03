7e5a87a67e6342c133617149b473730f3046be7b
Timo Furrer <tuxtimo@gmail.com>
2021-01-03 12:47:17 +0000

    Solve 2015 Day 9

---
+++ 2015/09/part2.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+with (Path(__file__).parent / "input.txt").open() as puzzle_input_file:
+    puzzle_input_raw = puzzle_input_file.read()
+
+import itertools
+import functools
+
+flights = {frozenset([x.split()[0], x.split()[2]]): int(x.split()[-1]) for x in puzzle_input_raw.splitlines()}
+destinations = set(itertools.chain(*{x for x in flights}))
+
+route_distances = {}
+for route in itertools.permutations(destinations, len(destinations)):
+    distance = functools.reduce(lambda d, f: d + flights[frozenset(f)], zip(route, route[1:]), 0)
+    route_distances[route] = distance
+
+shortest_route = max(route_distances.items(), key=lambda x: x[1])
+print(shortest_route)
