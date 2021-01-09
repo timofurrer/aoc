6dd2863bcb0e4ad7a0788bdbaf7d27a1c2b6ca51
Timo Furrer <tuxtimo@gmail.com>
2021-01-09 17:05:44 +0000

    Solve 2016 Day 6

---
+++ 2016/06/part2.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+puzzle_input_raw = (Path(__file__).parent / "input.txt").read_text()
+
+from collections import Counter
+
+columns = list(map(tuple, zip(*puzzle_input_raw.splitlines())))
+
+message = [Counter(c).most_common()[-1][0] for c in columns]
+print("".join(message))
