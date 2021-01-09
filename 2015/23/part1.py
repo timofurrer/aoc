110463efaca860980d257ef4d92a0ec1f81595ea
Timo Furrer <tuxtimo@gmail.com>
2021-01-09 11:38:56 +0000

    Solve 2015 Day 23

---
+++ 2015/23/part1.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+
+puzzle_input_raw = (Path(__file__).parent / "input.txt").read_text()
+
+from collections import defaultdict
+
+program = [tuple(x.strip(",") for x in x.split()) for x in puzzle_input_raw.splitlines()]
+
+registers = defaultdict(int)
+pointer = 0
+
+while pointer < len(program):
+    instr, *args = program[pointer]
+    if instr == "hlf":
+        registers[args[0]] //= 2
+        pointer += 1
+    elif instr == "tpl":
+        registers[args[0]] *= 3
+        pointer += 1
+    elif instr == "inc":
+        registers[args[0]] += 1
+        pointer += 1
+    elif instr == "jmp":
+        pointer += int(args[0])
+    elif instr == "jie":
+        if registers[args[0]] % 2 == 0:
+            pointer += int(args[1])
+        else:
+            pointer += 1
+    elif instr == "jio":
+        if registers[args[0]] == 1:
+            pointer += int(args[1])
+        else:
+            pointer += 1
+    else:
+        assert False, "unknown instruction"
+
+print(registers)
