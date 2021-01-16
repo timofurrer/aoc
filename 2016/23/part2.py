3041de3d8b558cc4f8c477cc62a37a746d0b9466
Timo Furrer <tuxtimo@gmail.com>
2021-01-16 17:45:17 +0000

    Solve 2016 Day 23

---
+++ 2016/23/part2.py
@@ -0,0 +1,58 @@
+from pathlib import Path
+
+puzzle_input_raw = (Path(__file__).parent / "input_part2.txt").read_text()
+
+import re
+
+DIGIT = re.compile(r"-?\d+")
+
+instructions = [x.split() for x in puzzle_input_raw.splitlines()]
+
+registers = {"a": 12}
+pointer = 0
+eval_param = lambda x: int(x) if DIGIT.fullmatch(x) else registers[x]
+while pointer < len(instructions):
+    instr, *args = instructions[pointer]
+    try:
+        if instr == "cpy":
+            value = eval_param(args[0])
+            registers[args[1]] = value
+            pointer += 1
+        elif instr == "inc":
+            registers[args[0]] += 1
+            pointer += 1
+        elif instr == "dec":
+            registers[args[0]] -= 1
+            pointer += 1
+        elif instr == "jnz":
+            if eval_param(args[0]) != 0:
+                pointer += eval_param(args[1])
+            else:
+                pointer += 1
+        elif instr == "tgl":
+            p = pointer + eval_param(args[0])
+            pointer += 1
+            if not (0 <= p < len(instructions)):
+                continue
+
+            name, *arguments = instructions[p]
+            if len(arguments) == 1:
+                if name == "inc":
+                    instructions[p] = ["dec"] + arguments
+                else:
+                    instructions[p] = ["inc"] + arguments
+            elif len(arguments) == 2:
+                if name == "jnz":
+                    instructions[p] = ["cpy"] + arguments
+                else:
+                    instructions[p] = ["jnz"] + arguments
+        elif instr == "mul":
+            registers[args[2]] = eval_param(args[0]) * eval_param(args[1])
+            pointer += 1
+        else:
+            assert False, "unknown"
+    except KeyError as exc:
+        pointer += 1
+
+
+print(registers["a"])
