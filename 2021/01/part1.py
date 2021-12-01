9cb03c728a964f2158d94b139ed49709adc47f10
Timo Furrer <tuxtimo@gmail.com>
2021-12-01 10:38:04 +0000

    Improve 2021 day 1

--- 2021/01/part1.py
+++ 2021/01/part1.py
@@ -4,13 +4,9 @@ with (Path(__file__).parent / "input.txt").open() as puzzle_input_file:
     puzzle_input_raw = puzzle_input_file.read()
 
 
-import functools
+import itertools
 
 measurements = [int(m) for m in puzzle_input_raw.split()]
 
-increases = functools.reduce(
-    lambda acc, x: (x, acc[1] + 1) if acc[0] < x else (x, acc[1]),
-    measurements,
-    (measurements[0], 0)
-)
-print(increases[1])
+increases = sum(y > x for x, y in itertools.pairwise(measurements))
+print(increases)
--- 2021/01/part2.py
+++ 2021/01/part2.py
@@ -4,17 +4,11 @@ with (Path(__file__).parent / "input.txt").open() as puzzle_input_file:
     puzzle_input_raw = puzzle_input_file.read()
 
 
-import functools
+import itertools
 
 measurements = [int(m) for m in puzzle_input_raw.split()]
 
-measurement_windows = list(
+increases = sum(y > x for x, y in itertools.pairwise(
     map(sum, zip(measurements, measurements[1:], measurements[2:]))
-)
-
-increases = functools.reduce(
-    lambda acc, x: (x, acc[1] + 1) if acc[0] < x else (x, acc[1]),
-    measurement_windows,
-    (measurement_windows[0], 0)
-)
-print(increases[1])
+))
+print(increases)
