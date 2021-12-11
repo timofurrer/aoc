67a775a61c6b41a034056d6f76a56c056b099a10
Timo Furrer <tuxtimo@gmail.com>
2021-12-11 19:01:30 +0100

    Simplify 2021 day 8 part 2

--- 2021/08/part2.py
+++ 2021/08/part2.py
@@ -18,15 +18,15 @@ for signal_patterns, output_values in data:
         has_overlaps = lambda x, n: x in wire_map and len(wire_map[x] & p) == n
         if len(p) in UNIQUE_SEGMENTS_TO_DIGIT:  # matching unique signal pattern (1, 4, 7, 8)
             wire_map[UNIQUE_SEGMENTS_TO_DIGIT[len(p)]] = p
-        elif len(p) == 5 and 2 not in wire_map and has_overlaps(4, 2):  # matching 2
+        elif len(p) == 5 and has_overlaps(4, 2):  # matching 2
             wire_map[2] = p
-        elif len(p) == 5 and 3 not in wire_map and has_overlaps(1, 2):  # matching 3
+        elif len(p) == 5 and has_overlaps(1, 2):  # matching 3
             wire_map[3] = p
-        elif len(p) == 5 and 5 not in wire_map and has_overlaps(4, 3) and has_overlaps(1, 1):  # matching 5
+        elif len(p) == 5 and has_overlaps(4, 3) and has_overlaps(1, 1):  # matching 5
             wire_map[5] = p
-        elif len(p) == 6 and 6 not in wire_map and has_overlaps(1, 1):  # matching 6
+        elif len(p) == 6 and has_overlaps(1, 1):  # matching 6
             wire_map[6] = p
-        elif len(p) == 6 and 9 not in wire_map and has_overlaps(3, 5):  # matching 9
+        elif len(p) == 6 and has_overlaps(3, 5):  # matching 9
             wire_map[9] = p
         elif len(patterns) == 0:  # matching 0
             wire_map[0] = p
