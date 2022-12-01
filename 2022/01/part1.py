4abbd22f3ebaab837743f2eff60c866abbb50a69
Timo Furrer <tuxtimo@gmail.com>
2022-12-01 08:12:50 +0100

    Add 2022 Day 1

---
+++ 2022/01/part1.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+with (Path(__file__).parent / "input.txt").open() as puzzle_input_file:
+    puzzle_input_raw = puzzle_input_file.read()
+
+
+elf_calories = [sum(int(c) for c in elf.splitlines()) for elf in puzzle_input_raw.split("\n\n")]
+
+print(max(elf_calories))
