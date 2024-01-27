47d0f8a219d0d115bc8703df853bed88d7d9d635
Timo Furrer <tuxtimo@gmail.com>
2024-01-27 19:39:46 +0100

    Solve 2015 Day 01

--- haskell/src/Y15/Day01.hs
+++ haskell/src/Y15/Day01.hs
@@ -2,14 +2,12 @@ module Y15.Day01 (solve) where
 
 -- Imports
 
-import Lib.Debug
-
 -- Input Parsing 
 
-type Input = [String]
+type Input = [Char]
 
 parseInput :: String -> Input
-parseInput = lines
+parseInput x = x
 
 -- Common
 
@@ -18,18 +16,30 @@ parseInput = lines
 --- Part 1
 
 part1 :: Input -> Int
-part1 input = 42
+part1 = go 0
+  where
+    go n [] = n
+    go n ('(' : xs) = go (n + 1) xs
+    go n (')' : xs) = go (n - 1) xs
+    go _ xs = error ("invalid input " ++ xs)
 
 --- Part 2
-          
+
 part2 :: Input -> Int
-part2 input = 42
+part2 = go 0 0
+  where
+    go _ _ [] = error "invalid input; reached end floor before basement"
+    go n f (x : xs)
+      | n == -1   = f
+      | x == '('  = go (n + 1) (f + 1) xs
+      | x == ')'  = go (n - 1) (f + 1) xs
+      | otherwise = error "invalid input"
 
 -- Main
 
 solve :: String -> IO (String, String)
 solve rawInput = do
-  let input = parseInput rawInput 
+  let input = parseInput rawInput
   putStrLn "Parsed Input:"
   print input
   putStrLn ""
